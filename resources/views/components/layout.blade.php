b9be841855739f63a473d532a69043e871598ff5
ch-hamzzyy <chhamzarehman2135@gmail.com>
2023-12-27 19:33:02 +0500

    Adding Mobile Menu

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -37,72 +37,9 @@
 
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
-        <nav class="md:flex md:justify-between md:items-center">
-            <div>
-                <a href="/">
-                    <img src="/images/hamza.png" alt="Hamzzyy" width="165" height="16">
-                </a>
-            </div>
-
-            <div class="mt-8 md:mt-0 flex items-center">
-                @auth
-                    <x-dropdown>
-                        <x-slot name="trigger">
-                            <button class="text-xs font-bold uppercase">
-                                Welcome, {{ auth()->user()->name }}!
-                            </button>
-                        </x-slot>
-
-                        @admin
-                            <x-dropdown-item
-                                href="/admin/posts"
-                                :active="request()->is('admin/posts')"
-                            >
-                                Dashboard
-                            </x-dropdown-item>
-
-                            <x-dropdown-item
-                                href="/admin/posts/create"
-                                :active="request()->is('admin/posts/create')"
-                            >
-                                New Post
-                            </x-dropdown-item>
-                        @endadmin
-
-                        <x-dropdown-item
-                            href="#"
-                            x-data="{}"
-                            @click.prevent="document.querySelector('#logout-form').submit()"
-                        >
-                            Log Out
-                        </x-dropdown-item>
-
-                        <form id="logout-form" method="POST" action="/logout" class="hidden">
-                            @csrf
-                        </form>
-                    </x-dropdown>
-                @else
-                    <a href="/register"
-                       class="text-xs font-bold uppercase hover:text-blue-600 {{ request()->is('register') ? 'text-blue-500' : '' }}">
-                        Register
-                    </a>
-
-                    <a href="/login"
-                       class="ml-6 text-xs font-bold uppercase hover:text-blue-600 {{ request()->is('login') ? 'text-blue-500' : '' }}">
-                        Log In
-                    </a>
-                @endauth
-
-                <a href="/contact"
-                   class="bg-blue-500 ml-3 rounded-full hover:bg-blue-600 text-xs font-semibold text-white uppercase py-3 px-5">
-                    Contact Us
-                </a>
-                <a href="#newsletter"
-                   class="bg-blue-500 ml-3 rounded-full hover:bg-blue-600 text-xs font-semibold text-white uppercase py-3 px-5">
-                    Subscribe
-                </a>
-            </div>
-        </nav>
+        <header>
+            <x-navbar></x-navbar>
+        </header>
 
         {{ $slot }}
 
